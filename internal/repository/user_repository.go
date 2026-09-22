911dea4c792addd5aab7b94be98827c3cd10febe
unknown <sanhiyo261@gmail.com>
2026-09-23 00:57:14 +0700

    fix: Query, Repisotory, Minor mistakes

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -3,6 +3,8 @@ package repository
 import (
 	"context"
 	"trackpocket/internal/model/domain"
+	"github.com/google/uuid"
+
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -20,35 +22,39 @@ func NewUserRepository(db *pgxpool.Pool) UserRepository {
 	return &UserRepositoryImpl{DB: db}
 }
 
-func (response *UserRepositoryImpl) Register(ctx context.Context, user domain.User) (domain.User, error) {
+func (repository *UserRepositoryImpl) Register(ctx context.Context, user domain.User) (domain.User, error) {
+	user.ID = uuid.New()
 	query := 
-	`INSERT INTO users (name, email, password_hash, is_Verified)
-	VALUES ($1 $2 $3 $4) 
-	RETURNING id, created_at, updated_at`
+	`INSERT INTO users (id, name, email, password_hash, is_Verified)
+	VALUES ($1, $2, $3, $4, $5) 
+	RETURNING created_at, updated_at`
 
-	err:=response.DB.QueryRow(ctx, query, user.Name, user.Email, user.PasswordHash).
-	Scan(&user.ID, user.CreatedAt, user.UpdatedAt)
-
+	err:=repository.DB.QueryRow(ctx, query,user.ID, user.Name, user.Email, user.PasswordHash, user.IsVerified).
+	Scan(
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
 	if err != nil {
 		return user, err
 	}
 	return user, nil
 }
 
-func (response *UserRepositoryImpl) FindByEmail(ctx context.Context, email string)(domain.User, error){
+func (repository *UserRepositoryImpl) FindByEmail(ctx context.Context, email string)(domain.User, error){
 	query := `
-	SELECT id, name, email, password_hash, is_verified, creted_at, updated_at
+	SELECT id, name, email, password_hash, is_verified, created_at, updated_at
 	FROM users WHERE email = $1`
 
 	var user domain.User
 
-	err := response.DB.QueryRow(ctx, query, user.Email).Scan(
+	err := repository.DB.QueryRow(ctx, query, email).Scan(
 		&user.ID,
+		&user.Name,
 		&user.Email,
 		&user.PasswordHash,
-		user.IsVerified,
-		user.CreatedAt,
-		user.UpdatedAt,
+		&user.IsVerified,
+		&user.CreatedAt,
+		&user.UpdatedAt,
 	)
 
 	if err != nil {
--- migration/000002_create_refresh_tokens_table.up.sql
+++ migration/000002_create_refresh_tokens_table.up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS refresh_token(
+CREATE TABLE IF NOT EXISTS refresh_tokens(
     id UUID         PRIMARY KEY,
     user_id UUID    NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     token_hash      VARCHAR(255) NOT NULL,
